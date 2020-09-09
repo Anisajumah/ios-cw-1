a7919d8df6f0e7b7c2b90bce3d423822f166664d
Anisajumah <70814404+Anisajumah@users.noreply.github.com>
2020-09-09 20:11:08 +0300

    done

---
+++ playground/CW-1-1.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var name = "Anisa"
+var height = 1.73
+var age = 17
+var hobby1 = "cooking"
+var hobby2 = "runnig"
+print ("hello my is  \(name) and my hobbies \(hobby1) and \(hobby2) my age is \(age) and my hieght is \(height) ")
+var friendname = "hamah"
+var friendheight = 1.65
+var friendage = 18
+var friendhobby1 = "drawing"
+var friendhobby2 = "cooking"
+var mutual = hobby1==friendhobby2
